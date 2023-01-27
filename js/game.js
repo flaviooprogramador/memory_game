ce132b2789a9b811a6d3a3a215de336e982b0c3c
Flavio de oliveira <flaviogamer7007@gmail.com>
2023-01-26 23:46:04 -0300

    criando função para cartas em js

--- js/game.js
+++ js/game.js
@@ -1,18 +1,43 @@
 const grid = document.querySelector('.grid')
 
-const createCard = () => {
-  const card = document.createElement('div')
-  const front = document.createElement('div')
-  const back = document.createElement('div')
+const characters = [
+  'beth',
+  'jerry',
+  'jessica',
+  'morty',
+  'pessoa-passaro',
+  'pickle-rick',
+  'rick',
+  'summer',
+  'meeseeks',
+  'scroopy',
+]
 
-  card.className = 'card'
-  front.className = 'face front'
-  back.className = 'face back'
+const createElement = (tag, className) => {
+  const element = document.createElement(tag)
+  element.className = className
+  return element
+}
+
+const createCard = () => {
+  const card = createElement('div', 'card')
+  const front = createElement('div', 'face front')
+  const back = createElement('div', 'face back')
 
   card.appendChild(front)
   card.appendChild(back)
 
   grid.appendChild(card);
+
+  return card
 }
 
-createCard()
+
+const loadGame = () => {
+  characters.forEach((characters) => {
+    const card = createCard()
+    grid.appendChild(card)
+  })
+}
+
+loadGame()
