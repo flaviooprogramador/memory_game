a37b4ad81eac247d58a019eb46c9bbab3311d8a3
Flavio de oliveira <flaviogamer7007@gmail.com>
2023-01-27 22:20:07 -0300

    Projeto finalizado...
    finalizei o projeto, coloquei varias funcoes no projeto, como mostrar o nome e o tempo, assim que acabar o jogo ele mostra seu nome e o tempo que vc ficou ate ganhar...

--- js/game.js
+++ js/game.js
@@ -1,5 +1,6 @@
 const grid = document.querySelector('.grid')
-
+const spanPlayer = document.querySelector('.player')
+const timer = document.querySelector('.timer')
 const characters = [
   'beth',
   'jerry',
@@ -27,7 +28,8 @@ const cheekEnGame = () =>{
   const disabledCards = document.querySelectorAll('.disabled-card')
 
   if(disabledCards.length === 20){
-    alert('Parabens, Você conseguiu!')
+    clearInterval(this.loop)
+    alert(`Parabens, ${spanPlayer.innerHTML}! Seu tempo foi: ${timer.innerHTML}`)
   }
 }
 
@@ -104,4 +106,19 @@ const loadGame = () => {
   })
 }
 
-loadGame()
+const startTimer = () => {
+
+  this.loop = setInterval(() => {
+    const currentTime = + timer.innerHTML;
+    timer.innerHTML = currentTime + 1;
+  }, 1000);
+
+}
+
+
+window.onload = () => {
+  spanPlayer.innerHTML = localStorage.getItem('player');
+  startTimer();
+  loadGame();
+}
+
